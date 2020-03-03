6adfe75ccd2fedc0408de6ee75932db8fbaa2993
ThomasLMartin <fan.cardinals@gmail.com>
2020-03-03 13:04:21 -0500

    Added Reply Objects

---
+++ GetSome.Models/ReplyModels/ReplyCreate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetSome.Models.ReplyModels
+{
+    public class ReplyCreate
+    {
+        [Required]
+        public string Title { get; set; }
+
+        [MaxLength(9999)]
+        public string Content { get; set; }
+    }
+}
---
+++ GetSome.Models/ReplyModels/ReplyListItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetSome.Models.ReplyModels
+{
+    public class ReplyListItem
+    {
+        public Guid UserId { get; set; }
+        public int ReplyId { get; set; }
+        public string Title { get; set; }
+    }
+}
